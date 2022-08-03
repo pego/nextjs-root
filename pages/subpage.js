df07a08a8e4abaa54d52e79a29ccc6d67b257ec7
Massimo Pegoraro <massimo.pegoraro@atoms.studio>
2022-08-03 15:29:33 +0200

    first commit

--- pages/subpage.js
+++ pages/subpage.js
@@ -13,13 +13,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Subfolder Project
+          Welcome to Root Project Subpage
         </h1>
 
         <div className={styles.description}>
          <ul>
-         <li><a href="/">Link to root project root page</a></li>
-         <li><a href="/subfolder/subpage">Link to subfolder project sub page</a></li>
+         <li><a href="/subfolder">Link to subfolder project root page</a></li>
+         <li><a href="/subpage">Link to root project sub page</a></li>
          </ul>
         </div>
       </main>
