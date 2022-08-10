bd9d2889aa8f0cee5a130d8856d6c69c9b579c34
Massimo Pegoraro <massimo.pegoraro@atoms.studio>
2022-08-10 16:29:03 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
+     {
+      "source": "/spring-summer-2023-fashion-show",
+      "destination": "https://nextjs-subfolder.vercel.app/spring-summer-2023-fashion-show",
+      },
       {
       "source": "/spring-summer-2023-fashion-show/:path*",
-      "destination": "https://nextjs-subfolder.vercel.app/spring-summer-2023-fashion-show/:path*"
+      "destination": "https://nextjs-subfolder.vercel.app/spring-summer-2023-fashion-show/:path*",
       },
     ]
   },
